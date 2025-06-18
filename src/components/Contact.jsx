0aa5aafe3dbb0a1315355425f387256c7b977ba1
Joseph Mokua <mokuaj42@gmail.com>
2025-06-18 21:36:37 +0300

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ export default function Contact() {
         <p className="contact-info">
           Have a project in mind or just want to say hello? Feel free to reach out!
         </p>
-        <form action="https://formsubmit.co/mokuaj42@gmail.com" method="POST"  className="contact-form" onSubmit={(e) => e.preventDefault()}>
+        <form className="contact-form" onSubmit={(e) => e.preventDefault()}>
           <input type="text" name="name" required>
           <input type="email" name="email" required>
           <textarea name="message" required></textarea>
